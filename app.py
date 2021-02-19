08467de928d301363bc5db0d0d87a3bd70bc87e4
noelpoo <xloel93@hotmail.com>
2021-02-19 10:52:43 +0800

    adding cloud firestore SDK

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import os
 import time
 import uuid
 import math
+import firebase_admin
 import pandas as pd
+from firebase_admin import credentials
+from firebase_admin import firestore
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS, cross_origin
@@ -29,91 +32,6 @@ answer_db = []
 # TODO - CREATE FETCHING BY COUNT
 # TODO - "correct" field to accept string or array (for MMCQ)
 # TODO - more enum for MMCQ question type
-
-
-# END-POINT FOR GETTING FULL LIST
-class Answers(Resource):
-    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('device_id', type=str, required=False)
-        parser.add_argument('session_id', type=str, required=False)
-        device_id = parser.parse_args().get('device_id')
-        session_id = parser.parse_args().get('session_id')
-        batch = []
-        if device_id and session_id:
-            for answer in answer_db:
-                if answer['device_id'] == device_id and answer['session_id'] == session_id:
-                    batch.append(answer)
-        elif device_id and not session_id:
-            for answer in answer_db:
-                if answer['device_id'] == device_id:
-                    batch.append(answer)
-        elif not device_id and session_id:
-            for answer in answer_db:
-                if answer['session_id'] == session_id:
-                    batch.append(answer)
-        elif not device_id and not session_id:
-            for answer in answer_db:
-                batch.append(answer)
-
-        if batch:
-            return {
-                "count": len(batch),
-                "answers": batch
-            }, 200
-        else:
-            return {
-                'message': 'no answer with matching device_id or session_id'
-            }, 404
-
-
-class Answer(Resource):
-    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    def post(self):
-        data = request.get_json(force=True)
-        answer_id = str(uuid.uuid4())
-        obj = {
-            'answer_id': answer_id,
-            'questions': data['question'],
-            'answer': data['answer'],
-            'is_correct': data['is_correct'],
-            'score': data['score'],
-            'answered_in': data['answered_in'],
-            'answered_time': round(time.time()),
-            'session_id': data['player']['session_id'],
-            'device_id': data['player']['device_id']
-        }
-        answer_db.append(obj)
-        print(obj)
-        return obj, 201 if obj else 403
-
-    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('answer_id', type=str, required=True)
-        answer_id = parser.parse_args().get('answer_id')
-        if answer_id:
-            answer = next(filter(lambda x: x['answer_id'] == answer_id, answer_db), None)
-            return {'answer': answer}, 200 if answer else 404
-        else:
-            return {'message': 'missing query param'}, 400
-
-    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    def delete(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('answer_id', type=str, required=True)
-        answer_id = parser.parse_args().get('answer_id')
-        has_delete = False
-        if answer_id:
-            for answer in answer_db:
-                if answer['answer_id'] == answer_id:
-                    answer_db.remove(answer)
-                    has_delete = True
-        if has_delete:
-            return {'message': 'succesfully deleted {}'.format(answer_id)}, 200
-        else:
-            return {'message': 'failed to find answer {}'.format(answer_id)}, 404
 
 
 class Questions(Resource):
@@ -198,6 +116,96 @@ class Question(Resource):
             return {"message": "failed to parse CSV, please make sure CSV format is correct"}, 400
 
 
+class Answer(Resource):
+
+    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+    def post(self):
+        data = request.get_json(force=True)
+        answer_id = str(uuid.uuid4())
+        obj = {
+            'answer_id': answer_id,
+            'questions': data['question'],
+            'answer': data['answer'],
+            'is_correct': data['is_correct'],
+            'score': data['score'],
+            'answered_in': data['answered_in'],
+            'answered_time': round(time.time()),
+            'session_id': data['player']['session_id'],
+            'device_id': data['player']['device_id']
+        }
+        answer_db.append(obj)
+        print(obj)
+        return obj, 201 if obj else 403
+
+    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('answer_id', type=str, required=True)
+        answer_id = parser.parse_args().get('answer_id')
+        if answer_id:
+            answer = next(filter(lambda x: x['answer_id'] == answer_id, answer_db), None)
+            if answer:
+                return {'answer': answer}, 200
+            else:
+                return {'message': 'answer_id not found'}, 404
+        else:
+            return {'message': 'missing query param'}, 400
+
+    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('answer_id', type=str, required=True)
+        answer_id = parser.parse_args().get('answer_id')
+        has_delete = False
+        if answer_id:
+            for answer in answer_db:
+                if answer['answer_id'] == answer_id:
+                    answer_db.remove(answer)
+                    has_delete = True
+        if has_delete:
+            return {'message': 'succesfully deleted {}'.format(answer_id)}, 200
+        else:
+            return {'message': 'failed to find answer {}'.format(answer_id)}, 404
+
+
+# END-POINT FOR GETTING FULL LIST
+class Answers(Resource):
+
+    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('device_id', type=str, required=False)
+        parser.add_argument('session_id', type=str, required=False)
+        device_id = parser.parse_args().get('device_id')
+        session_id = parser.parse_args().get('session_id')
+        batch = []
+        if device_id and session_id:
+            for answer in answer_db:
+                if answer['device_id'] == device_id and answer['session_id'] == session_id:
+                    batch.append(answer)
+        elif device_id and not session_id:
+            for answer in answer_db:
+                if answer['device_id'] == device_id:
+                    batch.append(answer)
+        elif not device_id and session_id:
+            for answer in answer_db:
+                if answer['session_id'] == session_id:
+                    batch.append(answer)
+        elif not device_id and not session_id:
+            for answer in answer_db:
+                batch.append(answer)
+
+        if batch:
+            return {
+                       "count": len(batch),
+                       "answers": batch
+                   }, 200
+        else:
+            return {
+                       'message': 'no answer with matching device_id or session_id'
+                   }, 404
+
+
 # TODO - CREATE QUESTION-FEEDS API
 
 def create_time_limit(diff, qn_type, qn):
@@ -210,6 +218,7 @@ def create_time_limit(diff, qn_type, qn):
     return round(base + diff_pt + type_pt + qn_pt)
 
 
+# CREATING ENDPOINTS
 api.add_resource(Question, '{}/question'.format(API_PATH))
 api.add_resource(Questions, '{}/questions'.format(API_PATH))
 api.add_resource(Answer, '{}/answer'.format(API_PATH))
