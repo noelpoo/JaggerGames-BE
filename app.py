12ed8d777e727131765a8e76262edd09dcf949b3
noelpoo <xloel93@hotmail.com>
2021-04-21 14:42:07 +0800

    remodelled all API resources

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import os
-import time
-import uuid
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_cors import CORS, cross_origin
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
 from config import *
+
+# IMPORTING API RESOURCES FROM MODELS
 from models.tag import TagsResource
 from models.question import QuestionResource, AllQuestionsResource
 from models.csv_parser import CsvParserResource
+from models.answer import AnswerResource, AllAnswersResource
 
 # FIREBASE DB
 if not firebase_admin._apps:
@@ -35,126 +36,15 @@ jwt = JWTManager(app)
 # TODO - create job to refresh tag_list in-memory
 
 
-class Answer(Resource):
-
-    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    def post(self):
-        data = request.get_json(force=True)
-        answer_id = str(uuid.uuid4())
-        obj = {
-            'answer_id': answer_id,
-            'questions': data['question'],
-            'answer': data['answer'],
-            'is_correct': data['is_correct'],
-            'score': data['score'],
-            'answered_in': data['answered_in'],
-            'answered_time': round(time.time()),
-            'session_id': data['player']['session_id'],
-            'device_id': data['player']['device_id']
-        }
-        db.collection(ANSWERS_FB_DB).document(answer_id).set(obj)
-        return obj, 201 if obj else 403
-
-    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('answer_id', type=str, required=True)
-        answer_id = parser.parse_args().get('answer_id')
-        if answer_id:
-            doc_ref = db.collection(ANSWERS_FB_DB).document(answer_id)
-            doc = doc_ref.get()
-            if doc.exists:
-                answer = doc.to_dict()
-                return {
-                           'answer': answer
-                       }, 200
-            else:
-                return {
-                           "message": "item does not exists",
-                           "code": 404
-                       }, 404
-        else:
-            return {
-                       "message": "malformed request parameter",
-                       "code": 400
-                   }, 400
-
-    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    def delete(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('answer_id', type=str, required=True)
-        answer_id = parser.parse_args().get('answer_id')
-        has_delete = False
-        if answer_id:
-            doc_ref = db.collection(ANSWERS_FB_DB).document(answer_id)
-            doc = doc_ref.get()
-            if doc.exists:
-                db.collection(ANSWERS_FB_DB).document(answer_id).delete()
-                has_delete = True
-            if has_delete:
-                return {'message': 'succesfully deleted {}'.format(answer_id)}, 200
-            else:
-                return {'message': 'failed to find answer {}'.format(answer_id)}, 404
-        else:
-            return {
-                       'message': 'malformed query param',
-                       'code': 400
-                   }, 400
-
-
-# END-POINT FOR GETTING FULL LIST
-class Answers(Resource):
-
-    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('device_id', type=str, required=False)
-        parser.add_argument('session_id', type=str, required=False)
-        device_id = parser.parse_args().get('device_id')
-        session_id = parser.parse_args().get('session_id')
-        batch = []
-        if device_id and session_id:
-            docs = db.collection(ANSWERS_FB_DB).where('session_id', '==', session_id).where('device_id', '==',
-                                                                                            device_id).stream()
-            for doc in docs:
-                doc_resp = doc.to_dict()
-                batch.append(doc_resp)
-        elif device_id and not session_id:
-            docs = db.collection(ANSWERS_FB_DB).where('device_id', '==', device_id).stream()
-            for doc in docs:
-                doc_resp = doc.to_dict()
-                batch.append(doc_resp)
-        elif not device_id and session_id:
-            docs = db.collection(ANSWERS_FB_DB).where('session_id', '==', session_id).stream()
-            for doc in docs:
-                doc_resp = doc.to_dict()
-                batch.append(doc_resp)
-        elif not device_id and not session_id:
-            docs = db.collection(ANSWERS_FB_DB).stream()
-            for doc in docs:
-                doc_resp = doc.to_dict()
-                batch.append(doc_resp)
-
-        if batch:
-            return {
-                       "count": len(batch),
-                       "answers": batch
-                   }, 200
-        else:
-            return {
-                       'message': 'no answer with matching device_id or session_id',
-                       'code': 404
-                   }, 404
-
-
-# TODO - CREATE QUESTION-FEEDS API
-
-# CREATING ENDPOINTS
+# ENDPOINTS FOR QUESTIONS
 api.add_resource(QuestionResource, '{}/question'.format(API_PATH))
 api.add_resource(AllQuestionsResource, '{}/questions'.format(API_PATH))
+# ENDPOINT FOR CSV PARSER API
 api.add_resource(CsvParserResource, '{}/csv_parser'.format(API_PATH))
-api.add_resource(Answer, '{}/answer'.format(API_PATH))
-api.add_resource(Answers, '{}/answers'.format(API_PATH))
+# ENDPOINT FOR ANSWERS
+api.add_resource(AnswerResource, '{}/answer'.format(API_PATH))
+api.add_resource(AllAnswersResource, '{}/answers'.format(API_PATH))
+# ENDPOINT FOR TAGS
 api.add_resource(TagsResource, '{}/tags'.format(API_PATH))
 
 if __name__ == "__main__":
