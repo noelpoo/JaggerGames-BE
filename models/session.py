199be732c202cde0b7e4e62b37a75e310578bb18
noelpoo <xloel93@hotmail.com>
2021-04-22 10:31:32 +0800

    added timestamps for sessions API

--- models/session.py
+++ models/session.py
@@ -10,7 +10,7 @@ class SessionResource(Resource):
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('device_id', required=True, type=str)
+        parser.add_argument('device_id', required=True, type=str, help='Device ID missing in query parameter')
         device_id = parser.parse_args().get('device_id')
 
         results = Answer.find_by_device_id(device_id)
@@ -20,18 +20,20 @@ class SessionResource(Resource):
                     'session_id': session,
                     'answer_ids': [
                         i.answer_id for i in Answer.find_by_session_and_device_id(session, device_id)
-                    ]
+                    ],
+                    'session_start_time':
+                        min([i.answered_time for i in Answer.find_by_session_and_device_id(session,
+                                                                                           device_id)]),
+                    'session_end_time':
+                        max([i.answered_time for i in Answer.find_by_session_and_device_id(session,
+                                                                                           device_id)])
                 } for session in list(set([result.session_id for result in results]))
             ]
 
-            if return_list:
-                return {
-                    'results': return_list
-                }, 200
-            else:
-                return {
-                    'message': 'no sessions found for device_id {}'.format(device_id)
-                }
+            return {
+                       'results': return_list
+                   }, 200
+
         else:
             return {
                 'message': 'no sessions found for device_id {}'.format(device_id)
